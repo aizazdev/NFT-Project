107a49e18f3565fde5615b5f8383c674ac8792f0
Aizaz Ahmad <aizazkhan.uos@gmail.com>
2022-12-01 18:13:48 -0800

    NFT website
    NFT WEBSITE design

---
+++ components/Navbar.jsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div
+      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+    >
+      <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white">
+        Briar System
+      </h2>
+      <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+        {/* <img
+            src="/headset.svg"
+            alt="headset"
+            className="w-[24px] h-[24px] object-contain"
+          /> */}
+        <span className="font-normal text-[16px] text-white">
+          Connect Wallet
+        </span>
+      </button>
+    </div>
+  </motion.nav>
+);
+
+export default Navbar;
---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/briar1.png',
+    title: 'Barking Briar ',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/briar2.png',
+    title: 'Black Eyes',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/Briar3.png',
+    title: 'Briar with Weapon',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/briar4.png',
+    title: 'Briar Knife',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/briar5.png',
+    title: 'Serius Cute',
+  },
+];
+
+export const startingFeatures = [
+  'Find a NFT that you like and mint it',
+  'Go to opensea and explore you NFT',
+  'Boom trade you NFT ',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'telegram',
+    url: '/telegram.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'discord',
+    url: '/discord.svg',
+  },
+];
